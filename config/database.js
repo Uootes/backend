e3ca1efc654106f82f66b58bc7b3d669bc28722b
muabiodun99 <Muabiodun101@gmail.com>
2025-05-26 13:41:40 +0100

    removed the url

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
-const url = process.env.MONGODB_URL || "mongodb+srv://uootes:6eA4ojHea5IFksQT@cluster0.65wppn2.mongodb.net/Uootes"
+const url = process.env.MONGODB_URL 
 
 mongoose.connect(url)
 .then(() =>
