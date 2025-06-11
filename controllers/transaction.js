aa0412cc48ea05d6ad23cdfb3ebd98eb04ff83ee
vibeofficial <ichioguchristopher98@gmail.com>
2025-06-11 13:48:43 +0100

    i created the exchanger wallet and transaction model

---
+++ controllers/transaction.js
@@ -0,0 +1,7 @@
+const transactionModel = require('../models/transaction');
+const userModel = require('../models/user');
+const exchangerModel = require('../models/exchanger')
+const userWallet = require('../models/userWallet');
+const exchangerWallet = require('../models/exchangerWallet');
+
+
