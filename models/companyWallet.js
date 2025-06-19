5354f7c0dda276afad3f77adf20f33e454d32a08
vibeofficial <ichioguchristopher98@gmail.com>
2025-06-19 13:08:15 +0100

    created function to split company's revenue

---
+++ models/companyWallet.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const companyWalletSchema = new mongoose.Schema({
+  revenueBalance: { type: Number, default: 0 },
+  companyBalance: { type: Number, default: 0 },
+  poolBalance: { type: Number, default: 0 },
+  accumulation: { type: Number, default: 0 },
+}, { timestamps: true });
+
+const companyWalletModel = mongoose.model('companyWallets', companyWalletSchema);
+
+module.exports = companyWalletModel;
