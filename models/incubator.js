4246a0fc8dc0aea6d84d1f585870d69a84467a93
chinasa056 <chinasaacha05@gmail.com>
2025-09-03 16:07:13 +0100

    feat: add incubator feature

---
+++ models/incubator.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const incubatorCardSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    tier: {
+        type: String,
+        enum: ['Bronze', 'Silver', 'Gold'],
+        required: true
+    },
+
+    cptAmount: { type: Number, required: true },  
+    gscWorth: { type: Number, required: true }, 
+
+    totalDuration: { type: Number, required: true },  
+    remainingTime: { type: Number, required: true },  
+
+    startedAt: { type: Date }, 
+    endsAt: { type: Date },   
+
+    status: {
+        type: String,
+        enum: ['locked','active', 'claimable', 'claimed'],
+        default: 'locked'
+    },
+}, { timestamps: true });
+
+const Incubator = mongoose.model('Incubator', incubatorCardSchema);
+
+module.exports = Incubator;
